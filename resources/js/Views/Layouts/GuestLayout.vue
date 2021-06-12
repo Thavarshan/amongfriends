a460503049302379704401e614a8837738d9c0cf
Thavarshan Thayananthajothy <tjthavarshan@gmail.com>
2021-06-12 19:17:32 +0530

    Update UI

--- resources/js/Views/Layouts/GuestLayout.vue
+++ resources/js/Views/Layouts/GuestLayout.vue
@@ -1,6 +1,6 @@
 <template>
     <!-- Main Content Area -->
-    <main class="py-4 bg-gray-100 min-hscreen" role="main">
+    <main class="py-4 min-hscreen" role="main">
         <div class="container mx-auto px-4 sm:px-6">
             <slot></slot>
         </div>
