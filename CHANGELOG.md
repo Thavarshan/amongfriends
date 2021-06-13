6941dbe4d2700239226f23e8c7c1e734f7c8292d
Thavarshan Thayananthajothy <tjthavarshan@gmail.com>
2021-06-13 06:19:03 +0530

    Add change log

---
+++ CHANGELOG.md
@@ -0,0 +1,20 @@
+# Release Notes
+
+## [v1.0.1 (2020-06-13)](https://github.com/emberfuse/scorch/compare/v1.0.0...v1.0.1)
+
+### Added
+
+- Payable trait
+- Chain cancel methods for linked resources
+- Expressive and robust markdown parser
+- Cancellable contract for resources
+
+### Changed
+
+- Changed the column type of profile_photo_path to string(2048)
+- Refactored all payment types to adhere to common payment contract
+- Update user guide
+
+## v1.0.0 (2020-06-12)
+
+Initial stable release.
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->string('phone')->nullable();
             $table->timestamp('email_verified_at')->nullable();
-            $table->text('profile_photo_path')->nullable();
+            $table->string('profile_photo_path', 2048)->nullable();
             $table->text('two_factor_secret')->nullable();
             $table->text('two_factor_recovery_codes')->nullable();
             $table->json('settings')->nullable();
